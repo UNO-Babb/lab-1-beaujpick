120e85a98328a5eb5a9bba4dc619297818fc8c2a
beaujpick <beaujpick@gmail.com>
2025-08-31 03:45:23 +0000

    Done with lab

--- MadLib.py
+++ MadLib.py
@@ -1,3 +1,4 @@
+
 #MadLib.py
 #Name:
 #Date:
@@ -6,11 +7,17 @@
 def main():
   print("Madlib")
   #Ask user for words
-
+noun1 = input("Enter a food: ")
+noun2 = input("Enter a place: ")
+noun3 = input("Enter a noun: ")
+noun4 = input("Enter a place: ")
+noun5 = input("Enter a verb: ")
+noun6= input("Enter a animal: ")
   #Print the story with the user supplied words.
-
-
-
+print("I absolutely despise to eat", noun1,".")
+print("I wanted to go to the", noun2,"so I took my favorite", noun3, "with me.")
+print("I got tired and figured I'd head back to the", noun4, "and lie down.")
+print("It was then I remembered I forgot to", noun5, "my", noun6, "!")
 #Call the main function if this is the file being run.
 if __name__ == '__main__':
     main()
